a3cf1aa7c87bed633da90f6453315bec7ead18dc
Patrick Limbaga <trek@Patricks-MacBook-Air.local>
2026-05-17 02:50:48 +0800

    x

--- auth-server.js
+++ auth-server.js
@@ -1956,11 +1956,6 @@ app.post(
   }
 );
 
-// fallback
-app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
-});
-
 app.get(
   '/test-webhook-replay',
   async (req, res) => {
@@ -2006,6 +2001,11 @@ app.get(
 
   }
 );
+
+// fallback
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on ${PORT}`);
