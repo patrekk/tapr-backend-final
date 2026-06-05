161ff3b4e76ff57af4c06d469082cac10001da96
Patrick Limbaga <trek@192.168.68.162>
2026-06-05 18:01:50 +0800

    x

--- auth-server.js
+++ auth-server.js
@@ -2990,7 +2990,12 @@ app.get(
     const { data, error } =
       await supabase
         .from('wallet_sync_logs')
-        .select('*')
+        .select(`
+          *,
+          merchants (
+            name
+          )
+        `)
         .not('status', 'is', null)
         .order('created_at', { ascending: false })
         .limit(10);
